b616072606c7e07279216347590f6f4b32f1a958
phyniaejiofor <phyniaejiofor90@gmail.com>
2020-11-04 17:46:26 +0100

    I changed the price tag on menu and organize your events

--- index.php
+++ index.php
@@ -203,7 +203,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/coconutr.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Coconut Rice</a><span>$5.95</span>
+              <a href="#">Coconut Rice</a><span>&#8358;1500</span>
             </div>
             <div class="menu-ingredients">
               Coconut with Fried Chicken and Vegetables
@@ -213,7 +213,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/nm.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Fried Noodles</a><span>$1.95</span>
+              <a href="#">Fried Noodles</a><span>&#8358;1000</span>
             </div>
             <div class="menu-ingredients">
               Fried Noodles with egg, fried red meat and vegetables
@@ -223,7 +223,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="assets/img/chickenp.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Chicken Pepper Soup</a><span>$7.95</span>
+              <a href="#">Chicken Pepper Soup</a><span>&#8358;2000</span>
             </div>
             <div class="menu-ingredients">
               Chicken with dried fish, stock fish and vegetables
@@ -233,7 +233,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/asalad.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">African Salad</a><span>$8.95</span>
+              <a href="#">African Salad</a><span>&#8358;1300</span>
             </div>
             <div class="menu-ingredients">
               African Salad with Fried Fish, Cow skin And Liver
@@ -243,7 +243,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="assets/img/egusii.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Egusi Soup</a><span>$9.95</span>
+              <a href="#">Egusi Soup</a><span>&#8358;1200</span>
             </div>
             <div class="menu-ingredients">
               Egusi Soup with Fufu, Semo or Eba
@@ -253,7 +253,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="assets/img/oha.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Oha Soup</a><span>$9.95</span>
+              <a href="#">Oha Soup</a><span>&#8358;1200</span>
             </div>
             <div class="menu-ingredients">
               Oha Soup with Fufu, Semo or Eba
@@ -263,7 +263,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="assets/img/cfp.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Nsala Soup</a><span>$9.95</span>
+              <a href="#">Nsala Soup</a><span>&#8358;1500</span>
             </div>
             <div class="menu-ingredients">
               Nsala Soup with Fufu, Semo or Eba
@@ -273,7 +273,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/bangarice.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">BangaJellofRice</a><span>$6.95</span>
+              <a href="#">BangaJellofRice</a<span>&#8358;1200</span>
             </div>
             <div class="menu-ingredients">
               Jellof Rice cooked with Banga, and Vegetables
@@ -283,7 +283,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/jellof.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Jellof Rice</a><span>$7.95</span>
+              <a href="#">Jellof Rice</a><span>&#8358;1200</span>
             </div>
             <div class="menu-ingredients">
               Jellof Rice with Cow skin
@@ -292,7 +292,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/cust.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Custard with Fried Egg and Bread </a><span>$2.95</span>
+              <a href="#">Custard with Fried Egg and Bread </a><span>&#8358;1000</span>
             </div>
             <div class="menu-ingredients">
               Custard with Fried Egg and Bread
@@ -301,7 +301,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="assets/img/pyam.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Yam Porridge</a><span>$5.95</span>
+              <a href="#">Yam Porridge</a><span>&#8358;1300</span>
             </div>
             <div class="menu-ingredients">
               Yam porridge with Fish and Vegetables
@@ -310,7 +310,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="assets/img/ppf.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Bony Fish Pepper Soup</a><span>$9.95</span>
+              <a href="#">Bony Fish Pepper Soup</a><span>&#8358;2000</span>
             </div>
             <div class="menu-ingredients">
               Bony Fish Pepper Soup with Ripe Plantain
@@ -319,7 +319,7 @@
           <div class="col-lg-6 menu-item filter-salads">
             <img src="assets/img/yamswe.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Yam with Egg Sauce</a><span>$3.95</span>
+              <a href="#">Yam with Egg Sauce</a><span>&#8358;800</span>
             </div>
             <div class="menu-ingredients">
               Yam with Egg Sauce
@@ -328,169 +328,7 @@
           <div class="col-lg-6 menu-item filter-salads">
             <img src="assets/img/yamvs.png" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Yam with Vegetable Sauce</a><span>$4.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Yam with Vegetable Sauce
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Why Us Section -->
-
-    <!-- ======= Menu Section ======= -->
-    <section id="menu" class="menu section-bg">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Menu</h2>
-          <p>Check Our Tasty Menu</p>
-        </div>
-
-        <div class="row" data-aos="fade-up" data-aos-delay="100">
-          <div class="col-lg-12 d-flex justify-content-center">
-            <ul id="menu-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-starters">African Soup</li>
-              <li data-filter=".filter-salads">Sauce</li>
-              <li data-filter=".filter-specialty">Specialty</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row menu-container" data-aos="fade-up" data-aos-delay="200">
-
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/coconutr.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Coconut Rice</a><span>$5.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Coconut with Fried Chicken and Vegetables
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/nm.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Fried Noodles</a><span>$1.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Fried Noodles with egg, fried red meat and vegetables
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-starters">
-            <img src="assets/img/chickenp.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Chicken Pepper Soup</a><span>$7.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Chicken with dried fish, stock fish and vegetables
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/asalad.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">African Salad</a><span>$8.95</span>
-            </div>
-            <div class="menu-ingredients">
-              African Salad with Fried Fish, Cow skin And Liver
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-starters">
-            <img src="assets/img/egusii.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Egusi Soup</a><span>$9.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Egusi Soup with Fufu, Semo or Eba
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-starters">
-            <img src="assets/img/oha.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Oha Soup</a><span>$9.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Oha Soup with Fufu, Semo or Eba
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-starters">
-            <img src="assets/img/cfp.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Nsala Soup</a><span>$9.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Nsala Soup with Fufu, Semo or Eba
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/bangarice.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">BangaJellofRice</a><span>$6.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Jellof Rice cooked with Banga, and Vegetables
-            </div>
-          </div>
-
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/jellof.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Jellof Rice</a><span>$7.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Jellof Rice with Cow skin
-            </div>
-          </div>
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/cust.jpg" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Custard with Fried Egg and Bread </a><span>$2.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Custard with Fried Egg and Bread
-            </div>
-          </div>
-          <div class="col-lg-6 menu-item filter-specialty">
-            <img src="assets/img/pyam.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Yam Porridge</a><span>$5.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Yam porridge with Fish and Vegetables
-            </div>
-          </div>
-          <div class="col-lg-6 menu-item filter-starters">
-            <img src="assets/img/ppf.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Bony Fish Pepper Soup</a><span>$9.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Bony Fish Pepper Soup with Ripe Plantain
-            </div>
-          </div>
-          <div class="col-lg-6 menu-item filter-salads">
-            <img src="assets/img/yamswe.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Yam with Egg Sauce</a><span>$3.95</span>
-            </div>
-            <div class="menu-ingredients">
-              Yam with Egg Sauce
-            </div>
-          </div>
-          <div class="col-lg-6 menu-item filter-salads">
-            <img src="assets/img/yamvs.png" class="menu-img" alt="">
-            <div class="menu-content">
-              <a href="#">Yam with Vegetable Sauce</a><span>$4.95</span>
+              <a href="#">Yam with Vegetable Sauce</a><span>&#8358;800</span>
             </div>
             <div class="menu-ingredients">
               Yam with Vegetable Sauce
@@ -630,7 +468,7 @@
             <div class="col-lg-6 pt-4 pt-lg-0 content">
               <h3>Birthday Parties</h3>
               <div class="price">
-                <p><span>$189</span></p>
+                <p><span>&#8358;20000</span></p>
               </div>
               <p class="font-italic">
                 We serve our  dishes with the passion to quench all tasty; ranging from the classic meals to the least our customers can afford
@@ -654,7 +492,7 @@
             <div class="col-lg-6 pt-4 pt-lg-0 content">
               <h3>Private Parties</h3>
               <div class="price">
-                <p><span>$290</span></p>
+                <p><span>&#8358;50000</span></p>
               </div>
               <p class="font-italic">
                 We serve our  dishes with the passion to quench all tasty; ranging from the classic meals to the least our customers can afford
@@ -678,7 +516,7 @@
             <div class="col-lg-6 pt-4 pt-lg-0 content">
               <h3>Custom Parties</h3>
               <div class="price">
-                <p><span>$99</span></p>
+                <p><span>&#8358;70000</span></p>
               </div>
               <p class="font-italic">
                 We serve our  dishes with the passion to quench all tasty; ranging from the classic meals to the least our customers can afford
