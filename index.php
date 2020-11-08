c1380bf91aafcb13826ef3ad92864d3ed676426d
phyniaejiofor <phyniaejiofor90@gmail.com>
2020-11-08 12:36:17 +0100

    i added new picture

--- index.php
+++ index.php
@@ -805,17 +805,17 @@
 
           <div class="col-lg-4 col-md-4">
             <div class="member">
-              <img src="assets/img/Amaka1.jpg" class="img-fluid rounded-circle" alt="">
+              <img src="assets/img/Camera Kiss.jpg" class="img-fluid rounded-circle" alt="">
               <div class="member-info">
                 <div class="member-info-content">
-                  <h4>Miss Ejiofor Phynia</h4>
-                  <spa>Master  Chef</spa></span>
+                  <h4>Mr. Mmadubuike Sunday Chukwuebuka</h4>
+                  <spa>Chief Photographer</spa></span>
                 </div>
                 <div class="social">
-                  <a href="https://twitter.com/phynia4"><i class="icofont-twitter" target></i></a>
-                  <a href="https://www.facebook.com/ejiofor.chiamaka.1"><i class="icofont-facebook"></i></a>
-                  <a href="https://www.instagram.com/amakanwam"><i class="icofont-instagram"></i></a>
-                  <a href="https://www.linkedin.com/in/chiamaka-ejiofor"><i class="icofont-linkedin"></i></a>
+                  <a href="https://twitter.com/sunnylearner"><i class="icofont-twitter" target></i></a>
+                  <a href="https://www.facebook.com/Sunny Len"><i class="icofont-facebook"></i></a>
+                  <a href="https://www.instagram.com/sunny_lenz"><i class="icofont-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/Sunday Chukwuebuka Madubuike"><i class="icofont-linkedin"></i></a>
                   
                 </div>
               </div>
@@ -831,10 +831,10 @@
                   <span>Chief Supervisor</span>
                 </div>
                 <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
+                  <a href="http://twitter.com/@AkachukwuNnorom"><i class="icofont-twitter"></i></a>
+                  <a href="https://web.facebook.com/akachukwu.precious.988/"><i class="icofont-facebook"></i></a>
+                  <a href="https://www.instagram.com/p/CHC-yxMp62s/?igshid=j8eun7vdsvqr"><i class="icofont-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/nnorom-precious-akachukwu-1284131a5"><i class="icofont-linkedin"></i></a>
                 </div>
               </div>
             </div>
@@ -852,7 +852,7 @@
                 <div class="social">
                   <a href="https://www.twitter.com/rexsteroxxy"><i class="icofont-twitter"></i></a>
                   <a href="https://www.facebook.com/amadi.austin.77"><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
+                  <a href="#"><i class="icofont-instagram"></i></a>
                   <a href="https://www.linkedin.com/in/amadiaustinchukwuemeka"><i class="icofont-linkedin"></i></a>
                 </div>
               </div>
@@ -879,17 +879,17 @@
 
           <div class="col-lg-4 col-md-4">
             <div class="member" >
-              <img src="assets/img/Akolite2.jpg " height=600" width="500" class="img-fluid rounded-circle" alt="">
+              <img src="assets/img/steve.jpg " height=600" width="500" class="img-fluid rounded-circle" alt="">
               <div class="member-info">
                 <div class="member-info-content">
-                  <h4>Nnorom Precious</h4>
+                  <h4>Mr. Uzondu Stephen</h4>
                   <span>Marketer</span>
                 </div>
                 <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
+                  <a href="https://www.twitter.com/StephenUzondu"><i class="icofont-twitter"></i></a>
+                  <a href="#"><i class="icofont-facebook"></i></a>
+                  <a href="https://www.instagram.com/uzondu_stephen"><i class="icofont-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/StephenUzondu"><i class="icofont-linkedin"></i></a>
                 </div>
               </div>
             </div>
@@ -1062,7 +1062,7 @@
       </div>
       <div class="credits">
       
-        Designed by <a href="https://bootstrapmade.com/">Techies</a>
+        Designed by <a href="https://bootstrapmade.com/">Phynia Chiamaka Ejiofor </a>
       </div>
     </div>
   </footer><!-- End Footer -->
