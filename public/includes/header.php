2639cada5b9164a37385ac436af39390566caa35
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-02-22 15:51:53 +0100

    code push on header

--- public/includes/header.php
+++ public/includes/header.php
@@ -59,7 +59,7 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">Phynia's Tasty Bites</a></h1>
+      <h1 class="logo mr-auto"><a href="index.php">Phynia's Tasty Bites</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
