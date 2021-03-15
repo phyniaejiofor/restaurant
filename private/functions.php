6ab4f5e754f5f978ab0d95506c43d91593156139
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-03-15 14:06:17 +0100

    code push

--- private/functions.php
+++ private/functions.php
@@ -1,5 +1,11 @@
 <?php
+function h($string=""){
+	return htmlspecialchars($string);
+    }
 
+    function redirect_to($location){
+        header('location:' . $location);
+        }
 function is_post_request(){
 	return $_SERVER['REQUEST_METHOD'] == 'POST';
     }
@@ -10,4 +16,63 @@ function is_get_request(){
     }
 
 
+
+    function display_errors($errors=array()) {
+        $output = '';
+        if(!empty($errors)) {
+          $output .= "<div class=\"bg-danger\">";
+          $output .= "Please fix the following errors:";
+          $output .= "<ul>";
+          foreach($errors as $error) {
+            $output .= "<li>" . h($error) . "</li>";
+          }
+          $output .= "</ul>";
+          $output .= "</div>";
+        }
+        return $output;
+        }
+
+
+
+ function validateBookATableValues($data){
+    $errors = [];
+
+    if ($data['name'] === "") {
+      $errors[] = "Name cannot be blank.";
+    }
+    
+    if ($data['email'] === "") {
+      $errors[] = "email cannot be blank.";
+     }
+    
+    
+     if ($data['phone'] === "") {
+      $errors[] = "phone cannot be blank.";
+     }
+    
+    
+     if ($data['date'] === "") {
+      $errors[] = "date cannot be blank.";
+     }
+    
+    
+     if ($data['time'] === "") {
+      $errors[] = "time cannot be blank.";
+     }
+    
+     if ($data['people'] === "") {
+      $errors[] = "people cannot be blank.";
+     }
+    
+     if ($data['message'] === "") {
+      $errors[] = "message cannot be blank.";
+     }
+
+
+     return $errors;
+    
+ }       
+
 ?>
+
+
--- public/index.php
+++ public/index.php
@@ -1,4 +1,48 @@
 <?php
+require_once("../private/initialize.php");
+// check if request is a post request
+ if (is_post_request()) {
+  
+
+  // collect form inputs
+  $data = [];
+  $data['name']= h($_POST['name']) ?? '';
+  $data['email'] = h($_POST['email']) ?? '';
+  $data['phone'] = h($_POST['phone']) ?? '';
+  $data['date'] = h($_POST['date']) ?? '';
+  $data['time'] = h($_POST['time']) ?? '';
+  $data['people'] = h($_POST['people']) ?? '';
+  $data['message'] = h($_POST['message']) ?? '';
+
+
+
+  
+
+  // validate user inputs
+$errors = validateBookATableValues($data);
+
+
+
+ if (empty($errors)) {
+// submit data to database
+
+// redirect back to home
+
+redirect_to('./index.php');
+ }
+
+
+
+  
+
+
+
+ }
+ 
+ 
+
+
+
 require_once("includes/header.php");
 
 require_once("home.php");
